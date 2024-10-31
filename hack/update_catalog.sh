cfd8ac6bb7ac9bb73a49a7cd46b5bb9cfd91f3b6
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-10-31 19:10:40 +0000

    chore(deps): update ocp-bpfman-operator-bundle to 648a424
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- hack/update_catalog.sh
+++ hack/update_catalog.sh
@@ -3,7 +3,7 @@ set -eu
 
 #!/usr/bin/env bash
 
-export BPFMAN_OPERATOR_BUNDLE_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-operator-bundle@sha256:9aa6bc30d0efb87a6892595a112833cab03be2f7323acf8c6c0af777b6fc6dee"
+export BPFMAN_OPERATOR_BUNDLE_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-operator-bundle@sha256:648a424b6dfe906e49a3549b16c0efa58ffb33e17eb91fa4da2ab02ac34d5485"
 export BPFMAN_OPERATOR_IMAGE_PULLSPEC="registry.redhat.io/bpfman/bpfman-rhel9-operator@sha256:eda490e58393be2d35bfe4583fa7a93a32017f5e58c2e53f4e6362e78e00f96d"
 
 export INDEX_FILE=/configs/bpfman-operator/index.yaml
